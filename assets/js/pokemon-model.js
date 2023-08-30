b9a519457147700c76ec24bdbb55870bd6bae9aa
PregoBS <rodrigocherutti@hotmail.com>
2023-08-30 13:18:41 -0300

    added more data to Pokemon class

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,60 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  constructor({
+    number,
+    name,
+    type,
+    types,
+    photo,
+    moves,
+    abilities,
+    baseExp,
+    stats,
+    weight,
+    height,
+  }) {
+    this.number = number;
+    this.name = name;
+    this.type = type;
+    this.photo = photo;
+    this.baseExp = baseExp;
+    this.weight = weight;
+    this.height = height;
+    this.stats = stats || [];
+    this.types = types || [];
+    this.moves = moves || [];
+    this.abilities = abilities || [];
+  }
+}
+
+class PokemonStat {
+  constructor({ baseStat, effort, name, url }) {
+    this.baseStat = baseStat;
+    this.effort = effort;
+    this.name = name;
+    this.url = url;
+  }
+}
+
+class PokemonType {
+  constructor({ slot, name, url }) {
+    this.slot = slot;
+    this.name = name;
+    this.url = url;
+  }
+}
+
+class PokemonMove {
+  constructor({ name, url }) {
+    this.name = name;
+    this.url = url;
+  }
+}
+
+class PokemonAbility {
+  constructor({ name, url, isHidden, slot }) {
+    this.name = name;
+    this.url = url;
+    this.isHidden = isHidden;
+    this.slot = slot
+  }
 }
