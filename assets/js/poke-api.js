7654a24818bc8b385eb464341fcec0eda870a24a
PregoBS <rodrigocherutti@hotmail.com>
2023-08-31 14:44:09 -0300

    refactoring poke-api to fetch more data

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,63 +1,136 @@
 const pokemonsCache = {};
 const pokeApi = {};
 
-function convertPokeApiDetailToPokemonAndCache(pokeDetail) {
-  const stats = pokeDetail.stats.map((statSlot) => (
-    new PokemonStat({
-      baseStat: statSlot.base_stat,
-      effort: statSlot.effort,
-      name: statSlot.stat.name,
-      url: statSlot.stat.url,
-    })
-  ));
+const POKEMONS_CACHE_KEY = "pokemons";
 
-  const types = pokeDetail.types.map((typeSlot) => (
-    new PokemonType({
-      slot: typeSlot.slot,
-      name: typeSlot.type.name,
-      url: typeSlot.type.url,
-    })
-  ));
+function writeCacheToLocalStorage() {
+  try {
+    localStorage.setItem(POKEMONS_CACHE_KEY, JSON.stringify(pokemonsCache));
+  } catch (err) {
+    console.log("[LOCAL STORAGE] Error caching pokemons:", err);
+  }
+}
 
-  const moves = pokeDetail.moves.map((moveSlot) => (
+function getPokemonsCache() {
+  try {
+    const cache = localStorage.getItem(POKEMONS_CACHE_KEY);
+    if (cache) return JSON.parse(cache);
+    else return null;
+  } catch (err) {
+    console.log("[LOCAL STORAGE] Error getting pokemons cache:", err);
+  }
+}
+
+function convertPokeApiDetailAndSpeciesToPokemonAndCache(
+  pokeDetail,
+  pokeSpecies
+) {
+  const types = pokeDetail.types.map(
+    (typeSlot) =>
+      new PokemonType({
+        slot: typeSlot.slot,
+        name: typeSlot.type.name,
+        url: typeSlot.type.url,
+      })
+  );
+
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
+  pokemon.type = types[0];
+  pokemon.types = types;
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  pokemon.weight = pokeDetail.weight;
+  pokemon.height = pokeDetail.height;
+  pokemon.baseExp = pokeDetail.base_experience;
+  pokemon.moves = pokeDetail.moves.map(
+    (moveSlot) =>
       new PokemonMove({
         name: moveSlot.move.name,
         url: moveSlot.move.url,
       })
-  ));
+  );
+  pokemon.abilities = pokeDetail.abilities.map(
+    (abilitySlot) =>
+      new PokemonAbility({
+        slot: abilitySlot.slot,
+        isHidden: abilitySlot.isHidden,
+        name: abilitySlot.ability.name,
+        url: abilitySlot.ability.url,
+      })
+  );
+  pokemon.stats = pokeDetail.stats.map(
+    (statSlot) =>
+      new PokemonStat({
+        baseStat: statSlot.base_stat,
+        effort: statSlot.effort,
+        name: statSlot.stat.name,
+        url: statSlot.stat.url,
+      })
+  );
 
-  const abilities = pokeDetail.abilities.map((abilitySlot) => (
-    new PokemonAbility({
-      slot: abilitySlot.slot,
-      isHidden: abilitySlot.isHidden,
-      name: abilitySlot.ability.name,
-      url: abilitySlot.ability.url,
-    })
-  ));
-
-  pokemonsCache[pokeDetail.name] = new Pokemon({
-    number: pokeDetail.id,
-    name: pokeDetail.name,
-    type: types[0],
-    baseExp: pokeDetail.base_experience,
-    weight: pokeDetail.weight,
-    height: pokeDetail.height,
-    photo: pokeDetail.sprites.other.dream_world.front_default,
-    types,
-    moves,
-    abilities,
-    stats
+  // from species
+  pokemon.baseHappiness = pokeSpecies.base_happiness;
+  pokemon.captureRate = pokeSpecies.capture_rate;
+  pokemon.genderRate = pokeSpecies.gender_rate;
+  pokemon.eggGroups = pokeSpecies.egg_groups.map(
+    (group) =>
+      new PokemonEggGroup({
+        name: group.name,
+        url: group.url,
+      })
+  );
+  pokemon.evolvesFromSpecies =
+    pokeSpecies.evolves_from_species !== null
+      ? new PokemonEvolvesFromSpecies({
+          name: pokeSpecies.evolves_from_species.name,
+          url: pokeSpecies.evolves_from_species.url,
+        })
+      : null;
+  pokemon.growthRate = new PokemonGrowthRate({
+    name: pokeSpecies.growth_rate.name,
+    url: pokeSpecies.growth_rate.url,
   });
-  
+  pokemon.habitat = new PokemonHabitat({
+    name: pokeSpecies.habitat.name,
+    url: pokeSpecies.habitat.url,
+  });
+  pokemon.hasGenderDifferences = pokeSpecies.has_gender_differences;
+  pokemon.isBaby = pokeSpecies.is_baby;
+  pokemon.isLegendary = pokeSpecies.is_legendary;
+  pokemon.isMythical = pokeSpecies.is_mythical;
+  pokemon.shape = new PokemonShape({
+    name: pokeSpecies.shape.name,
+    url: pokeSpecies.shape.url,
+  });
+  pokemon.varieties = pokeSpecies.varieties.map(
+    (variety) =>
+      new PokemonVariety({
+        name: variety.pokemon.name,
+        url: variety.pokemon.url,
+        isDefault: variety.is_default,
+      })
+  );
+
+  pokemonsCache[pokemon.name] = pokemon;
   return;
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
-  if (pokemonsCache[pokemon.name]) return;
+pokeApi.getPokemonDetail = (pokemonName) => {
+  if (pokemonsCache[pokemonName]) return;
 
-  return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemonAndCache);
+  const urls = [
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`,
+    `https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`,
+  ];
+
+  return Promise.all(
+    urls.map((url) => fetch(url).then((response) => response.json()))
+  )
+    .then(([pokeDetails, pokeSpecies]) => {
+      convertPokeApiDetailAndSpeciesToPokemonAndCache(pokeDetails, pokeSpecies);
+    })
+    .then(writeCacheToLocalStorage);
 };
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
@@ -66,6 +139,8 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((pokemons) =>
+      pokemons.map((pokemon) => pokeApi.getPokemonDetail(pokemon.name))
+    )
     .then((detailRequests) => Promise.all(detailRequests));
 };
